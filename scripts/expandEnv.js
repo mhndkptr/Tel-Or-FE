42ac07251016804602f988e1433fabff101a3889
mhndkptr <muhamadhendika77@gmail.com>
2025-05-13 06:54:25 +0700

    chore: adjust env script

--- scripts/expandEnv.js
+++ scripts/expandEnv.js
@@ -5,5 +5,7 @@ const childProcess = require("child_process");
 const myEnv = dotenv.config({ path: ".env" });
 dotenvExpand.expand(myEnv);
 
+console.log("APP_PORT:", process.env.APP_PORT);
+
 const script = process.argv.slice(2).join(" ");
 childProcess.execSync(script, { stdio: "inherit", env: process.env });
